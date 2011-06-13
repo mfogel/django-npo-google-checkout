e2d8ff307580c4737d022199ae940a8efbc4a574
Mike Fogel <mike@fogel.ca>
2011-06-13 15:42:04 -0700

    adding dt, file, lineno etc to default logging

--- npo_google_checkout/log.py
+++ npo_google_checkout/log.py
@@ -5,6 +5,14 @@ class FileRequestHandler(logging.FileHandler):
     Append the request body and any other useful info before emiting
     the log record.
     """
+
+    fmt = '%(levelname)-3.3s:%(asctime)s:%(filename)s:%(lineno)s: %(message)s'
+
+    def __init__(self, *args, **kwargs):
+        # the __init__ method of the parent sets self.formatter to None.
+        logging.FileHandler.__init__(self, *args, **kwargs)
+        self.setFormatter(logging.Formatter(self.fmt))
+
     def emit(self, record):
         raw_post_data = None
         if hasattr(record, 'request') and \
@@ -14,7 +22,7 @@ class FileRequestHandler(logging.FileHandler):
             raw_post_data = record.raw_post_data
 
         if raw_post_data:
-            record.msg = "\n -- {0}\n -- raw_post_data below:\n{1}". \
+            record.msg = "{0}\n{1}\n". \
                     format(record.msg, raw_post_data)
         # logging handler classes are still 'classic' classes, arg.
         return logging.FileHandler.emit(self, record)
